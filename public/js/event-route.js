f1ae3ad65991de71e9b175596cd61a99cb6aa305
Curt Newcomer <newcomercurt@gmail.com>
2016-06-26 13:42:20 -0700

    serve pages with express set up pagejs

---
+++ public/js/event-route.js
@@ -0,0 +1,14 @@
+page.base('/event');
+page('/', function() {
+  console.log('/');
+});
+page('/timing', function() {
+  console.log('/timing');
+});
+page('/status', function() {
+  console.log('/status');
+});
+page('/clusters', function() {
+  console.log('/clusters');
+});
+page();
--- src/app.js
+++ src/app.js
@@ -3,7 +3,19 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/',express.static('public'));
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile('/views/index.html',{root:__dirname + '/..'});
+});
+
+app.get('/event*', function(req, res){
+  res.sendFile('/views/event.html',{root:__dirname + '/..'});
+});
+
+app.get('/admin*', function(req, res){
+  res.sendFile('/views/admin.html',{root:__dirname + '/..'});
+});
 
 app.listen(app.get('port'), function() {
   console.log('The server is running on port', app.get('port'));
