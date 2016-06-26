8af73c0f745ca0e599cbf0d4bbc92b9adfa78977
libertinistic <libertinistic@gmail.com>
2016-06-26 14:45:43 -0700

    added coterminous graphs

--- sandboxes/two-chart-sandbox/script.js
+++ sandboxes/two-chart-sandbox/script.js
@@ -1,45 +1,67 @@
-var myPlot = document.getElementById('myDiv');
-//   d3 = Plotly.d3,
-//   N = 16,
-//   x = d3.range(N),
-//   y = d3.range(N).map( d3.random.normal() ),
-//   data = [ { x:x, y:y, type:'scatter',
-//           mode:'markers', marker:{size:16} } ],
-//   layout = {
-//     hovermode:'closest',
-//     title:'Click on Points'
-//   };
-//
-// Plotly.newPlot('myDiv', data, layout);
-var trace1 = {
+//Graphing Aggregate Data
+var aggGraph = document.getElementById('aggregateGraph');
+var aggTrace = {
   z: [[1, 20, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
   x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
   y: ['Morning', 'Afternoon', 'Evening'],
   type: 'heatmap'
 };
+var aggData = [aggTrace];
+Plotly.newPlot('aggregateGraph', aggData);
+//Graphing Personal Data //suspect that wrapping these in functions allows clearer namespace. Use FP in production version of code.
+var perGraph = document.getElementById('personalGraph');
+var perTrace = {
+  z: [[1, 0, 0, 0, 1], [0, 1, 0, 0, 0], [0, 0, 1, -1, 2]],
+  x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], //Are labels needed?
+  y: ['Morning', 'Afternoon', 'Evening'],
+  type: 'heatmap'
+};
+var perData = [perTrace];
+Plotly.newPlot('personalGraph', perData);
+//jQuery Animation //Standardize everything to jQ selections?
 
-var data = [trace1];
-Plotly.newPlot('myDiv', data);
+$('#personalGraph rect.drag.cursor-crosshair').hover(function() {
+  console.log('in');
+  $('#personalGraph').animate({
+    'opacity': '1',
+  }, 800);
+},function() {
+  console.log('out');
+  $('#personalGraph').animate({
+    'opacity': '0.0',
+  }, 800);
+});
 
-(function listen() {
-  myPlot.on('plotly_click', function(ctx){
-    console.log(ctx);
-    trace1.z[
-            ctx.points[0].pointNumber[0]
-            ][
-            ctx.points[0].pointNumber[1]
-            ] += 10;
-  // update = {
-  //   z: [[[1, 2, 3, 5, 1], [2, 1, 6, 8, 3], [3, 6, 1, -1, 2]]]
-  // };
-  // var update = {
-  //   'z':[[['1', '2', '3', '3', '2'], ['3', '1', '4', '6', '3'], ['2', '4', '1', '1', '-1']]]
-  // };
-  // console.log(trace1.z);
-  // Plotly.restyle(myPlot, update);
-    Plotly.purge(myPlot);
-  // Plotly.redraw(myPlot);
-    Plotly.plot('myDiv', data);
-    listen();
+//Event Listener to support updating and re-rendering graphs.
+(function userClick() {
+  personalGraph.on('plotly_click', function(event){
+    perData.z[
+      event.points[0].pointNumber[0]
+    ][
+      event.points[0].pointNumber[0]
+    ] += 10;
+    aggData.z[
+      event.points[0].pointNumber[0]
+    ][
+      event.points[0].pointNumber[0]
+    ] += 10;
+    Plotly.purge($('allgraphs'));
+    Plotly.newPlot('personalGraph', perData);
+    Plotly.newPlot('aggregateGraph', aggData);
+    userClick();
   });
 }) ();
+
+// (function listen() {
+//   myPlot.on('plotly_click', function(ctx){
+//     console.log(ctx);
+//     trace1.z[
+//             ctx.points[0].pointNumber[0]
+//             ][
+//             ctx.points[0].pointNumber[1]
+//             ] += 10;
+//     Plotly.purge(myPlot);
+//     Plotly.plot('myDiv', data);
+//     listen();
+//   });
+// }) ();
