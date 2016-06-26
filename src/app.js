3c6052dc3ad9b174c434ebe177d9acea90d670da
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-25 20:47:19 -0700

    get port by env variable

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use('/',express.static('public'));
 
-app.listen(3000, function() {
-  console.log('The server is running on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('The server is running on port', app.get('port'));
 });
