5dc0feb48237e6556d7ce041b948f72a30f3e963
libertinistic <libertinistic@gmail.com>
2016-06-28 16:16:18 -0700

    additional layout work HTML/CSS

--- public/js/calendar-view.js
+++ public/js/calendar-view.js
@@ -1,10 +1,21 @@
 $(function() {
-  var layout = {};
+  var layout = {
+
+  };
   var aggData = [{
-    z: [[1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1], [20, 1, 60, 80, 30, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1], [30, 60, 1, -10, 20, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1],[30, 60, 1, -10, 20, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1],[30, 60, 1, -10, 20, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1],[30, 60, 1, -10, 20, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1],[30, 60, 1, -10, 20, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1, 20, 30, 50, 1]],
+    z: [
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, 1, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, 3, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, 5, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+    ],
     x: ['1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a', '12a', '1p', '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p', '11p', '12p'],
-    y: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-    type: 'heatmap'
+    y: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    type: 'heatmap',
+    colorscale: 'YIGnBu',
   }];
   Plotly.newPlot('aggGraph', aggData, layout, {
     staticPlot: true,
@@ -19,7 +30,8 @@ $(function() {
   }];
   Plotly.newPlot('perGraph', perData, layout, {
     // staticPlot: 'true',
-    displayModeBar: false
+    displayModeBar: false,
+    hoverInfo: false
   });
   var $perGraph = $('#perGraph');
   $('#perGraph rect.drag.cursor-crosshair').hover(function() {
