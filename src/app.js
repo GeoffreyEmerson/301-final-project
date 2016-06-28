c71358a08c051dd986154bdfe391a72dcb55179f
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-27 22:22:19 -0700

    routing for page.js

--- src/app.js
+++ src/app.js
@@ -5,13 +5,17 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static('public'));
-
 require('./database');
 require('./seed');
 
+app.use('/', express.static('public'));
+
 app.use(parser.json());
 app.use('/api', router);
+
+app.get('*', function(req, res){
+  res.sendFile('/public/index.html',{root:__dirname + '/..'});
+});
 
 app.listen(app.get('port'), function() {
   console.log('The server is running on port', app.get('port'));
