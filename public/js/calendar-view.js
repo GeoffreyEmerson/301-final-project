ce78692c179aa054b869e38f8de57b182a3a71a9
libertinistic <libertinistic@gmail.com>
2016-06-29 11:13:23 -0700

    preliminary integration of highcharts with index

--- public/js/calendar-view.js
+++ public/js/calendar-view.js
@@ -1,6 +1,8 @@
 $(function() {
   calendarView = {};
 
+  var $container = $('#container');
+
   calendarView.render = function () {
     $container.highcharts({
       chart: {
@@ -33,6 +35,9 @@ $(function() {
         symbolHeight: 280
       },
       tooltip: {
+        formatter: function () {
+          return '<b> At ' + this.series.xAxis.categories[this.point.x] + ' on ' + this.series.yAxis.categories[this.point.y] + ':</b> <br> '; //TODO: List of guests with preference greater than 0.
+        }
         // formatter: function () {
         //   // console.log(this);
         //   return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> sold <br><b>' +
@@ -40,7 +45,7 @@ $(function() {
         // }
       },
       series: [{
-        name: 'Time Availability', //topidID goes here
+        name: 'Availability',
         borderWidth: 1,
         data: [[0, 0, null], [0, 1, null], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 30], [9, 4, 91], [23, 6, 150]],
         dataLabels: {
@@ -51,11 +56,12 @@ $(function() {
             console.log(event.point.series.xAxis.categories[event.point.x],
               event.point.series.yAxis.categories[event.point.y],
             event.point.series.name);
+            console.log(this.userOptions.id);
           },
         },
         // color: '#000000',
         borderWidth: 0,
-        id: 5//use this to pass data to send function?
+        id: 5 //TopicID
       }]
     });
 
