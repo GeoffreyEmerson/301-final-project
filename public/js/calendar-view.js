da7763f1a7818ab3914137053ffdd2b1c0d238fb
libertinistic <libertinistic@gmail.com>
2016-06-29 13:15:21 -0700

    assorted unsuccessful monkeying with hover interactions

--- public/js/calendar-view.js
+++ public/js/calendar-view.js
@@ -1,7 +1,21 @@
 $(function() {
   calendarView = {};
-
   var $container = $('#container');
+  var colors = [
+    '',
+    'lime',
+    'green',
+    'red'
+  ];
+  var aggData = [[0, 0, null], [0, 1, null], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 30], [9, 4, 91], [23, 6, 150]];
+
+  calendarView.colorTranslater = function(ele) {
+    if (ele[2] == 0 || ele[2] == 1 || ele[2] == 2) {
+      return ele[2];
+    } else {
+      return 3;
+    }
+  };
 
   calendarView.render = function () {
     $container.highcharts({
@@ -19,7 +33,7 @@ $(function() {
       },
       yAxis: {
         categories: ['Sunday', 'Saturday', 'Friday', 'Thursday', 'Wednesday', 'Tuesday', 'Monday'],
-        title: null
+        title: null,
       },
       colorAxis: {
         min: 0,
@@ -47,26 +61,82 @@ $(function() {
       series: [{
         name: 'Availability',
         borderWidth: 1,
-        data: [[0, 0, null], [0, 1, null], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 30], [9, 4, 91], [23, 6, 150]],
+        data: aggData,
         dataLabels: {
           enabled: false,
         },
         events:  {
           click: function() {
-            console.log(event.point.series.xAxis.categories[event.point.x],
+            //Sends relevant information to backend.
+            console.log(
+              event.point.series.xAxis.categories[event.point.x],
               event.point.series.yAxis.categories[event.point.y],
-            event.point.series.name);
-            console.log(this.userOptions.id);
+              event.point.series.name,
+              this.userOptions.id);
+            //Receives information
+            //TODO: Write the ability to receive info here.
+            //Sets vote-state hover color.
+            //TODO: update local copy of vote-state array
           },
+          // mouseOver: function() {
+          //   console.log(this);
+          //   this.update({
+          //     color: 'red'
+          //   });
+          // },
+          // mouseOut: function() { //Appears to be unnecessary.
+          //   console.log(this);
+          //   this.update({
+          //     color: 'FFFFFF'
+          //   });
+          // }
         },
-        // color: '#000000',
+        // color: colors[],
+        // colors[calendarView.colorTranslater(aggData[ ])], //TODO: Write local pref array. Also, find a way to reference the correct member of that array.
         borderWidth: 0,
-        id: 5 //TopicID
-      }]
+        id: 5, //TODO: TopicID
+        index: 1
+      },
+//start of second data serie
+      {
+        name: 'aggData2',
+        borderWidth: 1,
+        data: aggData.map(function(ele) {
+          return [ele[0], ele[1], Math.random() * 125];
+        }),
+        dataLabels: {
+          enabled: false,
+        },
+        events:  {
+        },
+        borderWidth: 0,
+        id: 5, //TODO: TopicID
+        index: 1
+      }
+    ]
     });
 
     chart = $container.highcharts();
   };
-
   calendarView.render(); //this should be invoked elsewhere.
+
+  $('g.highcharts-series-group').hover(
+    function() {
+      console.log('in');
+      console.log(chart.series[0].index);
+      chart.series[0].index = '';
+      console.log(chart.series[0].index);
+      // chart.yAxis.reversedStacks = true;
+      // chart.redraw();
+      calendarView.render(); //this should be invoked elsewhere.
+
+    },
+    function () {
+      console.log('out');
+    });
+  // $('.highcharts-series-0').on('click', 'rect', function() {
+  //   console.log(this);
+  //   $(this).css('background-color', 'red !important');
+  // });
+
 });
