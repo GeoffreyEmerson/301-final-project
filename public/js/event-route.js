7b3fc1fa7746facb514d71b5d5289bbef4a1936c
Curt Newcomer <newcomercurt@gmail.com>
2016-06-29 12:19:33 -0700

    added name page, button for input, and auto advance on click

--- public/js/event-route.js
+++ public/js/event-route.js
@@ -7,38 +7,76 @@ function logRoute(ctx, next) {
 
 var $homepage = $('#homepage, #navigation');
 var $event = $('#event');
+var $name = $('#name, #navigation');
 var $admin = $('#event');
 var $404 = $('#not-found');
 
 page.base('/');
 page('*', logRoute);
+
 page('/', function() {
   showPage($homepage);
 });
 
 page('event', function() {
   showPage($event);
+  $('#googleAPI').show();
   triggerMapResize();
+});
+
+page('name', function() {
+  showPage($name);
 });
 
 page('event/timing', function() {
   showPage($event);
   $('#timing').show();
+  $('#googleAPI').hide();
 });
 
 page('event/status', function() {
   showPage($event);
   $('#status-content').show();
+  $('#googleAPI').show();
 });
 
 page('event/clusters', function() {
   showPage($event);
   $('#cluster').show();
+  $('#googleAPI').show();
+});
+page('event/add', function() {
+  showPage($event);
+  $('#add').show();
+  $('#googleAPI').show();
 });
 //right now the admin page is just set to be equal with the event page. leaving the code
 //below in case we need to use it.
 page('admin', function() {
   showPage($admin);
+});
+
+//gets text input from the event submission form and logs it to page and advances to name page
+$('#create-event').on('click', function(event){
+  event.preventDefault();
+  var eventValue = $('#event-value').val();
+  console.log(eventValue);
+  location = '/name';
+});
+
+//gets text input from the name submission form and logs it to page and advances to event page
+$('#create-name').on('click', function(event){
+  event.preventDefault();
+  var nameValue = $('#name-value').val();
+  console.log(nameValue);
+  location = '/event';
+});
+
+$('#add-topic').on('click', function(event){
+  event.preventDefault();
+  var topicValue = $('#topic').val();
+  console.log(topicValue);
+  location = '/event/clusters';
 });
 
 page('admin/timing', function() {});
