51ed358cb004b1382183df098ffc45d136a966a5
libertinistic <libertinistic@gmail.com>
2016-06-30 13:48:34 -0700

    added all front-end pieces of status button

--- public/js/home-view.js
+++ public/js/home-view.js
@@ -1,7 +1,30 @@
 (function(module) {
   var homeView = {};
+  var $tatus = $('#status');
   homeView.initHomeView = function () {
     $('#event-name').text($('#event').data(eventName));
   };
+  $tatus.on('click', function() {
+    console.log('ping');
+    //Cycle through a collection of states.
+    if ($tatus.hasClass('blank')) { //If blank -> maybe
+      $tatus.toggleClass('blank maybe');
+      $tatus.children().text('Maybe');
+      $('#status-instructions').remove();
+      //Send state to backend
+    } else if ($tatus.hasClass('maybe')) { //If maybe -> going
+      $tatus.toggleClass('maybe approve');
+      $tatus.children().text('Yep');
+      //Send state to backend
+    } else if ($tatus.hasClass('approve')) { //if going -> not going
+      $tatus.toggleClass('approve disapprove');
+      $tatus.children().text('Nope');
+      //Send state to backend
+    } else if ($tatus.hasClass('disapprove')) { //if not going -> blank
+      $tatus.toggleClass('disapprove blank');
+      $tatus.children().text('Going?');
+      //Send state to backend
+    }
+  });
   module.homeView = homeView;
 })(window);
