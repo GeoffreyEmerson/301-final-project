2021ae011f277b38b08255352d684b6d726f0aab
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-29 17:43:19 -0700

    halfway through integration

--- public/js/topic-controller.js
+++ public/js/topic-controller.js
@@ -12,14 +12,44 @@ function wordMe(wordName, userHash, topicHash) {
 // wordMe('peppers', 'lksaf9pwurp2o', 'we09r20lksjdf');
 
 //NOTE: In sand box context wordMe fails to invoke jQcloud properly when called by this handler more than once; calling wordMe directly from code works fine as per above; problem may not persist once pulling wordArr from storage.
-function newOpt() {
+function topicCloudInit() {
+  console.log('Initiating jQCloud');
+  $('#cloud').jQCloud([], {
+    width: 500,
+    height: 350
+  });
+
+  console.log('Initiating word cloud click handler');
   $('#creator').on('click', function() {
-    console.log('newOpt start');
+    console.log('new word created');
     var text = $('#wdText').val();
     var uHash = $('#userHash').val();
     var tHash = $('#topicHash').val();
-    wordMe(text, uHash, tHash);
-    console.log('newOpt complete');
+    wordClick(text, uHash, tHash);
+    console.log('end of word cloud initiation');
   });
+
+  function wordClick(text, userHashArg, topicIdArg, callback) {
+    $.ajax({
+      url: '/api/votes',
+      type: 'POST',
+      data: {name: text, userHash: userHashArg, topicId: topicIdArg},
+      cache: false
+    })
+    .done( function (data) {
+      // call the callback function here
+      console.log('Successful ajax call:');
+      console.log(data);
+      // data will return an array of word objects like this: {text:"lorem",weight:5,voteState:-1};
+      $('#cloud').jQCloud('update', data);
+      // update voteStates
+    })
+    .fail( function(jqXHR, textStatus, errorThrown) {
+      console.log('Failed to send click to database.');
+      // call the error version of the callback if any
+    });
+  }
+
 };
-newOpt();
+
+topicCloudInit();
