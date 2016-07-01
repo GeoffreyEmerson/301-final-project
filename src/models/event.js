4bdde44ca5d377d07c0908042b2f1001e3296741
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-30 23:57:06 -0700

    added a few fields to the event model

--- src/models/event.js
+++ src/models/event.js
@@ -4,7 +4,11 @@ var eventSchema = new mongoose.Schema({
   name: String, // "Bob's Shindig"
   hash: String, // "3JHG63JKH6G3J4HG6" // a URL safe hash that identifies the event
   organizer: [String], // ["2dhf5dyd6d83v63vxmv724"] // One or more user hashes
-  skin: String // "Night"
+  skin: String, // "Night"
+  location: String,
+  date: String,
+  times: String,
+  description: String
   // _id: "577079c2583eab030090ae97"
 });
 
