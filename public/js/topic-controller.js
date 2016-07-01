b99c0e60e25b28adff7766f288cd720a69762ef1
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-07-01 12:57:17 -0700

    word cloud gimpy but showing

--- public/js/topic-controller.js
+++ public/js/topic-controller.js
@@ -4,6 +4,21 @@
 
   var wordArr = [];
 
+  // Example of needed format for each word object
+  // {
+  //   text: 'BBQ',
+  //   weight: 1,
+  //   html: {
+  //     'data-usid': '8280592847bfc49f7f29d5015889caa7bec15024',
+  //     'data-topicid': 'we09r20lksjdf',
+  //     'data-votestate': 1,
+  //     'class' : 'noSelect'
+  //   }
+  // }
+
+  // Populate wordArr from database
+  //var
+
   // TopicController.wordMe = function(wordName, userHash, topicHash) {
   //   var newWd = new Word(wordName, userHash, topicHash);
   //   console.log(newWd);
@@ -43,7 +58,7 @@
     $('#creator').on('click', function() {
       console.log('new word created');
       var text = $('#wdText').val();
-      var uHash = $('#userHash').val();
+      var uHash = $('#user-id').data('userhash');
       var tHash = $('#topicHash').val();
       TopicController.wordClick(text, uHash, tHash);
       console.log('end of word cloud initiation');
@@ -51,6 +66,8 @@
   };
 
   TopicController.wordClick = function(text, userHashArg, topicIdArg, callback) {
+    console.log('We are voting on an option!');
+    console.log(text, userHashArg, topicIdArg, callback);
     $.ajax({
       url: '/api/votes',
       type: 'POST',
@@ -58,36 +75,60 @@
       cache: false
     })
       .done( function (data) {
-        // call the callback function here
+        console.log('Successful ajax call:');
+        console.log(data);
         $.ajax({
           url: '/api/votes',
           type: 'GET',
-          data: {name: text, userHash: userHashArg, topicId: topicIdArg},
           cache: false
+        })
+        .done(function(result){
+          console.log('full query result:',result);
+          // calculate aggregate weight
+          var filteredVotes = result.votes.filter(function(vote){
+            if (vote.topicId == topicIdArg && vote.name == text) return true;
+          });
+          console.log('filtered query result:',filteredVotes);
+          var aggregatedWeight = filteredVotes.reduce(function(prev,cur){
+            return prev + cur.weight;
+          },0);
+          console.log('The new aggregated weight is',aggregatedWeight);
+
+          wordInsert = {
+            text: data.vote.name,
+            weight: aggregatedWeight,
+            html: {
+              'data-usid': data.vote.userHash,
+              'data-topicid': data.vote.topicId,
+              'data-votestate': data.vote.weight,
+              'class' : 'noSelect'
+            }
+          };
+          console.log('New wordInsert:',wordInsert);
+
+          console.log('Pre-mapped wordArr:',wordArr);
+          var found = false;
+          wordArr = wordArr.map(function(wordObj){
+            if(wordObj.text == wordInsert.text) {
+              found = true;
+              return wordInsert;
+            }
+            return wordObj;
+          });
+
+          if (!found) wordArr.push(wordInsert);
+
+          console.log('New word array is',wordArr);
+          // data will return an array of word objects like this: {text:"lorem",weight:5,voteState:-1};
+          $('#cloud').jQCloud('update', wordArr);
+          // update voteStates
         });
-        console.log('Successful ajax call:');
-        console.log(data);
-        wordInsert = [{
-          text: data.vote.name,
-          weight: data.vote.weight,
-          html: {
-            'data-usid': data.vote.userHash,
-            'data-topicid': data.vote.topicId,
-            'data-votestate': 0,
-            'class' : 'noSelect'
-          }
-        }];
-        wordArr.push(wordInsert);
-// data will return an array of word objects like this: {text:"lorem",weight:5,voteState:-1};
-        $('#cloud').jQCloud('update', wordArr);
-        // update voteStates
       })
       .fail( function(jqXHR, textStatus, errorThrown) {
         console.log('Failed to send click to database.');
         // call the error version of the callback if any
       });
   };
-
 
   // DONE: find the right place to run topicCloudInit();
   // topicController.newOpt = function() {
